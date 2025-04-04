5359ed6e776166c0203826271e1ec9ff7b6017ca
Moln1kas <moln1qkas@gmail.com>
2025-04-04 17:10:41 +0300

    Initial commit

---
+++ apps/backend/target/debug/deps/unicode_xid-8716a69316f5e17c.d
@@ -0,0 +1,8 @@
+D:\Programming\sachsen\backend\target\debug\deps\libunicode_xid-8716a69316f5e17c.rmeta: C:\Users\moln1kas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-xid-0.2.6\src\lib.rs C:\Users\moln1kas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-xid-0.2.6\src\tables.rs
+
+D:\Programming\sachsen\backend\target\debug\deps\libunicode_xid-8716a69316f5e17c.rlib: C:\Users\moln1kas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-xid-0.2.6\src\lib.rs C:\Users\moln1kas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-xid-0.2.6\src\tables.rs
+
+D:\Programming\sachsen\backend\target\debug\deps\unicode_xid-8716a69316f5e17c.d: C:\Users\moln1kas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-xid-0.2.6\src\lib.rs C:\Users\moln1kas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-xid-0.2.6\src\tables.rs
+
+C:\Users\moln1kas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-xid-0.2.6\src\lib.rs:
+C:\Users\moln1kas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-xid-0.2.6\src\tables.rs:
